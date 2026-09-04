9706224fcd1f98e14c0469e10d741315f2670331
jeevanrajsurya <jeevanrajsurya@gmail.com>
2026-09-05 00:32:40 +0530

    admin url changed

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/admin/',
+  base: '/aurevix/',
   plugins: [react()],
   server: {
     port: 3001,
